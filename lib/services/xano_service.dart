b5b672f2bd78252cdc10e7af13c905ba54ab7d91
SamuelReinaldoRamirez <samuel.reinaldo.ramirez@gmail.com>
2025-06-28 21:04:02 +0200

    page de login good

--- lib/services/xano_service.dart
+++ lib/services/xano_service.dart
@@ -13,27 +13,37 @@ class XanoService {
   //   }
   //   return Platform.environment['XANO_API_KEY'] ?? 'YOUR_XANO_API_KEY';
   // }
-  static String get apiKey {
+
+  // static String get apiKey {
+  //   if (kIsWeb) {
+  //     throw UnsupportedError('Les variables d\'environnement ne sont pas supportées sur le web');
+  //   }
+  //   final key = dotenv.env['XANO_ACCESS_TOKEN'];
+  //   if (key == null || key.isEmpty) {
+  //     throw Exception('XANO_ACCESS_TOKEN non défini dans .env');
+  //   }
+  //   return key;
+  // }
+
+  static String get loginKey {
     if (kIsWeb) {
       throw UnsupportedError('Les variables d\'environnement ne sont pas supportées sur le web');
     }
-    final key = dotenv.env['XANO_API_KEY'];
+    final key = dotenv.env['XANO_LOGIN'];
     if (key == null || key.isEmpty) {
-      throw Exception('XANO_API_KEY non défini dans .env');
+      throw Exception('XANO_LOGIN non défini dans .env');
     }
     return key;
   }
 
 
   Future<Map<String, dynamic>> login(String email, String password) async {
-    print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaa");
-    print(apiKey);
     try {
       final response = await http.post(
         Uri.parse('$baseUrl/auth/login'),
         headers: {
           'Content-Type': 'application/json',
-          'x-api-key': apiKey,
+          'Authorization': loginKey,
         },
         body: jsonEncode({
           "email": email,
@@ -70,13 +80,9 @@ class XanoService {
     try {
       final response = await http.post(
         Uri.parse('$baseUrl/auth/register'),
-        // headers: {
-        //   'Content-Type': 'application/json',
-        //   'x-api-key': apiKey,
-        // },
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': 'Bearer $apiKey', // <- ici
+          // 'Authorization': 'Bearer $apiKey',
         },
         body: jsonEncode({
           "email": email,
@@ -101,190 +107,23 @@ class XanoService {
     }
   }
 
-
-  // // Future<bool> login(String email, String password) async {
-  // //   try {
-  // //     final response = await http.post(
-  // //       Uri.parse('$baseUrl/user'),
-  // //       headers: {
-  // //         'Content-Type': 'application/json',
-  // //         'x-api-key': apiKey,
-  // //       },
-  // //       body: jsonEncode({
-  // //         'email': email,
-  // //         'password': password,
-  // //       }),
-  // //     );
-
-  // //     if (response.statusCode == 200) {
-  // //       return true;
-  // //     }
-  // //     return false;
-  // //   } catch (e) {
-  // //     print('Erreur lors de la connexion: $e');
-  // //     return false;
-  // //   }
-  // // }
-
-
-  // Future<Map<String, dynamic>> login(String email, String password) async {
+  // Future<Map<String, dynamic>?> getUser(String email) async {
   //   try {
-  //     final response = await http.post(
-  //       Uri.parse('$baseUrl/auth/login'),
+  //     final response = await http.get(
+  //       Uri.parse('$baseUrl/user?email=$email'),
   //       headers: {
   //         'Content-Type': 'application/json',
   //         'x-api-key': apiKey,
   //       },
-  //       body: jsonEncode({
-  //         "email": email,
-  //         "pwd": password,
-  //       }),
   //     );
 
-  //     if (response['statusCode'] == 200) {
+  //     if (response.statusCode == 200) {
   //       return jsonDecode(response.body);
-  //       // return response.body.authToken;
-  //       // return true;
   //     }
-  // //     return null;
-  // //   } catch (e) {
-  // //     print('Erreur lors de la connexion: $e');
-  // //     return null;
-  // //   }
-  // // }
-  //     else {
-  //         return {
-  //           'success': false,
-  //           'statusCode': response.statusCode,
-  //           'message': 'Erreur lors de la connexion',
-  //           'body': response.body
-  //         };
-  //       }
-  //     } catch (e) {
-  //       return {
-  //         'success': false,
-  //         'message': 'Exception lors de la connexion',
-  //         'error': e.toString(),
-  //       };
-  //     }
+  //     return null;
+  //   } catch (e) {
+  //     print('Erreur lors de la récupération de l\'utilisateur: $e');
+  //     return null;
   //   }
-
-
-  //   Future<Map<String, dynamic>> register(String email, String password, [String? pseudo]) async {
-  //     try {
-  //       final response = await http.post(
-  //         Uri.parse('$baseUrl/auth/register'),
-  //         headers: {
-  //           'Content-Type': 'application/json',
-  //           'x-api-key': apiKey,
-  //         },
-  //         body: jsonEncode({
-  //           "email": email,
-  //           "pwd": password,
-  //           if (pseudo != null) "pseudo": pseudo,
-  //         }),
-  //       );
-
-  //       return {
-  //         'statusCode': response.statusCode,
-  //         'body': jsonDecode(response.body),
-  //       };
-  //     } catch (e) {
-  //       return {
-  //         'statusCode':  response.statusCode,
-  //         'error': e.toString(),
-  //       };
-  //     }
-  //   }
-
-
-
-  // // Future<Map<String, dynamic>> register(String email, String password, [String? pseudo]) async {
-  // //   try {
-  // //     final response = await http.post(
-  // //       Uri.parse('$baseUrl/auth/signup'),
-  // //       headers: {
-  // //         'Content-Type': 'application/json',
-  // //         'x-api-key': apiKey,
-  // //       },
-  // //       body: jsonEncode({
-  // //         "email": email,
-  // //         "pwd": password,
-  // //         "pseudo": pseudo
-  // //       }),
-  // //     );
-
-  // //     if (response['statusCode'] == 200) {
-  // //       return jsonDecode(response.body);
-  // //       // return response.body.authToken;
-  // //       // return true;
-  // //     }
-  // // //     return null;
-  // // //   } catch (e) {
-  // // //     print('Erreur lors de la connexion: $e');
-  // // //     return null;
-  // // //   }
-  // // // }
-  // // else {
-  // //         return {
-  // //           'success': false,
-  // //           'statusCode': response.statusCode,
-  // //           'message': 'Erreur lors de la connexion',
-  // //           'body': response.body
-  // //         };
-  // //       }
-  // //     } catch (e) {
-  // //       return {
-  // //         'success': false,
-  // //         'message': 'Exception lors de la connexion',
-  // //         'error': e.toString(),
-  // //       };
-  // //     }
-  // //   }
-
-
-  // // // Future<bool> register(String email, String password, String pseudo) async {
-  // // //   try {
-  // // //     final response = await http.post(
-  // // //       Uri.parse('$baseUrl/user/register'),
-  // // //       headers: {
-  // // //         'Content-Type': 'application/json',
-  // // //         'x-api-key': apiKey,
-  // // //       },
-  // // //       body: jsonEncode({
-  // // //         'email': email,
-  // // //         'password': password,
-  // // //         'pseudo': pseudo,
-  // // //       }),
-  // // //     );
-
-  // // //     if (response.statusCode == 200) {
-  // // //       return true;
-  // // //     }
-  // // //     return false;
-  // // //   } catch (e) {
-  // // //     print('Erreur lors de l\'inscription: $e');
-  // // //     return false;
-  // // //   }
-  // // // }
-
-  Future<Map<String, dynamic>?> getUser(String email) async {
-    try {
-      final response = await http.get(
-        Uri.parse('$baseUrl/user?email=$email'),
-        headers: {
-          'Content-Type': 'application/json',
-          'x-api-key': apiKey,
-        },
-      );
-
-      if (response.statusCode == 200) {
-        return jsonDecode(response.body);
-      }
-      return null;
-    } catch (e) {
-      print('Erreur lors de la récupération de l\'utilisateur: $e');
-      return null;
-    }
-  }
+  // }
 }
